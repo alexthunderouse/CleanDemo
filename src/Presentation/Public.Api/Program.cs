537c147ba01063a5845d44a50b73649ae5306248
abolkvadze <abolkvadze@UP-DEV-109>
2026-01-20 18:14:40 +0400

    fixed Scalar documentation

--- src/Presentation/Public.Api/Program.cs
+++ src/Presentation/Public.Api/Program.cs
@@ -2,6 +2,7 @@ using CleanAPIDemo.API.Configuration;
 using CleanAPIDemo.API.Middleware;
 using CleanAPIDemo.Application;
 using CleanAPIDemo.Infrastructure;
+using Scalar.AspNetCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +18,7 @@ builder.Services
     .AddResilienceConfiguration()
     .AddOpenTelemetryConfiguration()
     .AddHealthCheckConfiguration()
+    .AddOpenApi()
     .AddControllers();
 
 var app = builder.Build();
@@ -26,6 +28,15 @@ app.UseMiddleware<GlobalExceptionMiddleware>();
 app.UseHealthCheckConfiguration();
 app.UseSerilogRequestLoggingConfiguration();
 app.UseHttpsRedirection();
+
+// Map OpenAPI and Scalar documentation
+app.MapOpenApi();
+app.MapScalarApiReference(options =>
+{
+    options.WithTitle("CleanAPIDemo API")
+           .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+});
+
 app.UseAuthorization();
 app.MapControllers();
 
